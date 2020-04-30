a6a209ff4f177e18c27e39e6edb05a5cf563a659
baraskarharshal <baraskarharshal@gmail.com>
2020-05-01 02:07:03 +0530

    5 files modified, 6 added, 4 deleted

---
+++ src/app/file-types.config.ts
@@ -0,0 +1,31 @@
+export const FileTypes = [
+    'jpe',
+    'jpeg',
+    'jpg',
+    'png',
+    'gif',
+    'bmp',
+    'doc',
+    'docs',
+    'xls',
+    'xlsx',
+    'pdf',
+    'ppt',
+    'pptx',
+    'tiff',
+    'tif',
+    'html',
+    'htm',
+    'txt',
+    'zip',
+    'msg'
+];
+
+export const ImageTypes = [
+    'jpe',
+    'jpeg',
+    'jpg',
+    'png',
+    'gif',
+    'bmp',
+];
--- src/app/simple-file-upload/simple-file-upload.component.ts
+++ src/app/simple-file-upload/simple-file-upload.component.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {ImageTypes} from '../file-types.config';
 
 @Component({
   selector: 'app-simple-file-upload',
   templateUrl: './simple-file-upload.component.html',
-  styleUrls: ['./simple-file-upload.component.css']
+  styleUrls: ['./simple-file-upload.component.scss']
 })
 export class SimpleFileUploadComponent implements OnInit {
 
   imageUrl: string | ArrayBuffer;
   file: File;
+  isLoading = false;
   constructor() { }
 
   ngOnInit() {
   }
 
   onSelect(event) {
+    if(!this.isImage(event.target.files[0].name)) {
+      alert('Not a valid image file. Please upload image file.');
+      return;
+    }
+    this.isLoading = true;
     console.log(event);
     this.file = event.target.files[0];
     this.showPreview();
   }
 
+  isImage(fileName: string) {
+    const fileType = fileName.substring(fileName.lastIndexOf('.') + 1);
+    return ImageTypes.includes(fileType.toLocaleLowerCase());
+  }
+
   showPreview() {
+    this.imageUrl = '';
     let reader = new FileReader();
     reader.onload = (e) => {
-      this.imageUrl = e.target['result'];
+      setTimeout(() => {
+        this.imageUrl = e.target['result'];
+        this.isLoading = false;
+      }, 500)
     };
     reader.readAsDataURL(this.file);
   }
